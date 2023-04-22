206cf327990f536c3a7b838bb7c7459a75db3239
RichardLeite <rs9010482@gmail.com>
2023-04-22 17:39:07 -0300

    Criado novo comando de links

---
+++ commands/links.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+
+async function getExternalIp() {
+  try {
+    const response = await axios.get('https://api.ipify.org?format=json');
+    return response.data.ip;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+const exampleEmbed = new EmbedBuilder()
+	.setColor("Purple")
+	.setTitle("Links")
+	.setDescription("Links para acessar os nossos Serviços")
+	.addFields(
+    { name: '\u200B', value: '\u200B' },
+    { name: 'Jellyfin', value: `[Clique aqui para assistir Filmes, Animes, Séries, Cursos, entre outros.](www)`, inline: true },
+    { name: '\u200B', value: '\u200B' },
+    { name: 'Jellyseerr', value: `[Clique aqui para solicitar algum contéudo a ser adicionado no Jellyfin.](www)`, inline: true },
+    { name: '\u200B', value: '\u200B' },
+    { name: 'Jfa-Go', value: `[Clique aqui para criar sua conta e acessar os serviços anteriormente mencionados.](www)`, inline: true },
+    { name: '\u200B', value: '\u200B' }
+  )
+  .setFooter({ text: 'Caso tenha algum problema fale com o FUNDADOR ou STAFF.' });
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("links")
+    .setDescription("Links para acessar os nossos Serviços"),
+
+  async execute(interaction) {
+    const ip = await getExternalIp();
+    exampleEmbed.spliceFields(1, 1, { name: 'Jellyfin', value: `[Clique aqui para assistir Filmes, Animes, Séries, Cursos, entre outros.](https://${ip}:8096)`, inline: true });
+    exampleEmbed.spliceFields(3, 1, { name: 'Jellyseerr', value: `[Clique aqui para solicitar algum contéudo a ser adicionado no Jellyfin.](https://${ip}:5055)`, inline: true });
+    exampleEmbed.spliceFields(5, 1, { name: 'Jfa-Go', value: `[Clique aqui para criar sua conta e acessar os serviços anteriormente mencionados.](https://${ip}:8056)`, inline: true });
+    await interaction.reply({ embeds: [exampleEmbed] })
+  }
+}
